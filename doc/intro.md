fadb7f774077c97f8d9a40f397e865f897a78c17
bh34e5 <b.hester0614@yahoo.com>
2023-10-20 22:42:21 -0400

    Adds initial files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-grapher
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clj_grapher/gui.clj
@@ -0,0 +1,5 @@
+;;; FIXME: I've already forgotten the syntax for :require... :')
+(ns clj-grapher.gui
+  (:gen-class)
+  (:require '(utils.core)))
+
